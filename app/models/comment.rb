7ac7f5e89c7b2b811c603820e6ae5dc104e2ade2
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-24 19:42:06 +0800

    made changes requested by previous reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_comments_counter
+
+  private
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
