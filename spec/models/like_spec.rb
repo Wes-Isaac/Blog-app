8828182ceeb284a3415ff48e24a172e261dd3d8a
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-26 06:46:06 +0800

    Create test for like

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  context 'test for the comments mode should pass' do
+    new_user = User.new(name: 'isaac', bio: 'whome', postscounter: 2)
+    new_post = Post.new(author: new_user, text: 'Nice post', comments_counter: 3, likes_counter: 5)
+    subject do
+      Like.new
+    end
+
+    before { subject.save }
+
+    it 'should return 4' do
+      subject.post = new_post
+      subject.author = new_user
+      subject.update_likes_counter
+      expect(subject.post.likes_counter).to be(6)
+    end
+  end
+end
