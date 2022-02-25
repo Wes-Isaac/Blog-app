b6f2e3d3f268f03df9e21f916a0aed48b78ab49c
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-26 06:39:37 +0800

    Create test for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'test for the User mode should pass' do
+    subject do
+      User.new
+    end
+
+    before { subject.save }
+
+    it 'should return nil' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should return nil' do
+      subject.posts_counter = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should return nil' do
+      subject.posts_counter = 2.5
+      expect(subject).to_not be_valid
+    end
+
+    describe 'User model should pass' do
+      before { 6.times { |_post| Post.new(author: subject, title: 'testing', text: 'testing') } }
+      it 'should pass these test' do
+        expect(subject.recent_posts).to eq(subject.posts.last(3))
+      end
+    end
+  end
+end
