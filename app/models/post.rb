15b234fb9ee28294bd5e2b057fda18e610d2289e
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-25 17:47:59 +0800

    Setup

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
+
   after_save :update_posts_counter
 
   private
 
   def update_posts_counter
     author.increment!(:posts_counter)
-  end
-
-  def recent_comments
-    comments.limit(5).order(created_at: :desc)
   end
 end
