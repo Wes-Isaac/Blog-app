7910c14b899bf6cc2a8ad7aa0b999f57c9afccc2
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-26 04:44:48 +0800

    Create likescontroller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,19 @@
+class LikesController < ApplicationController
+  def create
+    user_post = Post.find(params[:post_id])
+    user = User.find(params[:user_id])
+    new_like = Like.new(author: user, post: user_post)
+    new_like.update_likes_counter
+
+    respond_to do |format|
+      format.html do
+        if new_like.save
+          flash[:success] = 'Post Liked!'
+          redirect_to user_post_path(current_user.id, user_post.id)
+        else
+          render :new, alert: 'Error occured!'
+        end
+      end
+    end
+  end
+end
