c3cd543d62c77ace78eb98c8806a13f90bb3847f
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-26 06:48:47 +0800

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
-  
 
   validates :text, presence: true, length: { in: 1...100 }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  
   validates :name, presence: true
   validates :posts_counter, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
