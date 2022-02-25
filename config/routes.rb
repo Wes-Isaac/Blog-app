1c2dfb672d3e21e14fdd1f29a6975ee4c9a0fa22
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-26 02:52:44 +0800

    Create routes for like and comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index create new show] do
+      resources :comment, only: %i[create]
+      resources :like, only: %i[create]
   end
   root to: 'users#index'
   # Defines the root path route ("/")
