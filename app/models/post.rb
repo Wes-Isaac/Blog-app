bb091206f27001eec78ff8aedc02203cf1efad06
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-26 04:13:28 +0800

    Add form to create a new post

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,10 +7,6 @@ class Post < ApplicationRecord
     comments.limit(5).order(created_at: :desc)
   end
 
-  after_save :update_posts_counter
-
-  private
-
   def update_posts_counter
     author.increment!(:posts_counter)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index create new show] do
-      resources :comment, only: %i[create]
-      resources :like, only: %i[create]
+      resources :comments, only: %i[create]
+      resources :likes, only: %i[create]
     end
   end
   root to: 'users#index'
