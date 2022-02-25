41c909d3f17705412186eebf428115b66b969182
Wes-Isaac <yisehakmitiku@gmail.com>
2022-02-26 03:10:33 +0800

    Create new and create actions in comments_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,31 @@ class PostsController < ApplicationController
     @user = @post.author
     @comments = @post.comments
   end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.new(post_params)
+    post.likes_counter = 0
+    post.comments_counter = 0
+    post.update_posts_counter
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:success] = 'New post created successfully'
+          redirect_to user_post_path(current_user.id, post.id)
+        else
+          render :new, alert :'Error!'
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:data).permit(:title, :text)
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :posts, only: %i[index create new show] do
       resources :comment, only: %i[create]
       resources :like, only: %i[create]
+    end
   end
   root to: 'users#index'
   # Defines the root path route ("/")
